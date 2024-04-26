7935288d13c372411fdaaa2e004b3aa673e6a5af
k0lesan <k0lesan2k00@gmail.com>
2024-04-26 21:42:04 +0300

    core: install vite and typescript

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@utilities": path.resolve(__dirname, "./src/utilities"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
+});
