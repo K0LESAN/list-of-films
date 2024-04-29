1fc2405da33e87249afa437e63a5009ef0d3b402
K0LESAN <k0lesan2k00@gmail.com>
2024-04-30 02:08:58 +0300

    fix: correct url for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/list-of-films'>
       <App />
     </BrowserRouter>
   </StrictMode>
