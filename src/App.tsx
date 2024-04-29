35a0d6613c44c217404e20e91fb2fabe9960e2de
K0LESAN <k0lesan2k00@gmail.com>
2024-04-29 23:33:57 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import List from './pages/List/List';
+import Container from './components/Container/Container';
+
 const App = () => {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Container>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Navigate
+                to='/1'
+                replace
+              />
+            }
+          />
+          <Route
+            path='/:id'
+            element={<List />}
+          />
+        </Routes>
+      </Container>
     </>
   );
 };
