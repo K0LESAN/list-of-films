0ad988cfb8db28f552bb4ecbf6dbda3e993fefed
K0LESAN <k0lesan2k00@gmail.com>
2024-04-30 00:22:42 +0300

    refactor: edit component

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,13 @@
+import styles from './List.module.scss';
+import ListFilms from '@/components/ListFilms/ListFilms';
+
+const Main = () => {
+  return (
+    <>
+      <h1 className={styles.title}>Список фильмов</h1>
+      <ListFilms height={600}></ListFilms>
+    </>
+  );
+};
+
+export default Main;
