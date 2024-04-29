44b132be8b915d0fc7fae7277f7c2978f623301e
K0LESAN <k0lesan2k00@gmail.com>
2024-04-29 23:32:52 +0300

    feat: add container

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,11 @@
+import styles from './Container.module.scss';
+
+const Container = ({ children }: React.PropsWithChildren) => {
+  return (
+    <>
+      <div className={styles.container}>{children}</div>
+    </>
+  );
+};
+
+export default Container;
