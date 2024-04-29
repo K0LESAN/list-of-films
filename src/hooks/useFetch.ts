99147ef8c05fbbcb9a60e43be4260381c0a65523
K0LESAN <k0lesan2k00@gmail.com>
2024-04-29 22:40:47 +0300

    feat: useFetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,50 @@
+import { IFetchOptions, IQueryParams } from '@/types/api.interface';
+import { useEffect, useState } from 'react';
+
+function useFetch<T>(
+  fetcher: ((fetchOptions: IFetchOptions) => Promise<T>) | string,
+  queryParams: IQueryParams = {},
+  ...depedencies: unknown[]
+): [T | undefined, boolean] {
+  const [data, setData] = useState<T>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const controller: AbortController = new AbortController();
+    const request: () => Promise<void> = async (): Promise<void> => {
+      setIsLoading(true);
+
+      try {
+        const data: T | Response =
+          typeof fetcher === 'function'
+            ? await fetcher({
+                signal: controller.signal,
+                queryParams
+              })
+            : await fetch(fetcher, {
+                method: 'GET',
+                signal: controller.signal,
+                headers: {
+                  accept: 'application/json'
+                }
+              });
+
+        setData(data instanceof Response ? await data.json() : data);
+      } catch (error: unknown) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    request();
+
+    return (): void => {
+      controller.abort('Clear useFetch!');
+    };
+  }, depedencies);
+
+  return [data, isLoading];
+}
+
+export default useFetch;
